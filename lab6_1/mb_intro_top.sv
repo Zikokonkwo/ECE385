133f4b631b717975a8835446ecffac93b7a804b6
JasonHart922 <113485657+JasonHart922@users.noreply.github.com>
2024-10-15 18:54:26 -0500

    Update mb_intro_top.sv

--- lab6_1/mb_intro_top.sv
+++ lab6_1/mb_intro_top.sv
@@ -17,11 +17,15 @@ module mb_intro_top
     output logic uart_rxd,
     input  logic uart_txd);
 
-  mb_block mb_block_i//defined from wrapper
+  mb_block mb_block_i//below is defined from wrapper
        (.clk_100MHz(clk),
-        .gpio_rtl_0_tri_o(led[0]),
+        .gpio_rtl_0_tri_o(led[15:0]),
+        .gpio_rtl_1_tri_i(sw[15:0]),
+        .gpio_rtl_2_tri_i(btn[1]),
         .reset_rtl_0(~btn[0]),      //Note the inversion of the reset button. Buttons are active low, but the MicroBlaze reset is active high
         .uart_rtl_0_rxd(uart_txd),  //Note the switcheroo between RTX and TXD. This is a common source of confusion in embedded development
-        .uart_rtl_0_txd(uart_rxd)); //RXD = Received Data, and TXD = Transmitted Data, but whether data is transmitted or received depeneds on the
-                                    //perspective. Here, the TXD port means transmitted by the FPGA (but received by the Urbana Board's UART chip)
+        .uart_rtl_0_txd(uart_rxd) //RXD = Received Data, and TXD = Transmitted Data, but whether data is transmitted or received depeneds on the
+                                   //perspective. Here, the TXD port means transmitted by the FPGA (but received by the Urbana Board's UART chip)
+        
+            );
 endmodule
