dd12b2bb2f4a9d9901bbb43839704a0e195a912a
JasonHart922 <113485657+JasonHart922@users.noreply.github.com>
2024-10-15 18:54:12 -0500

    Update Vitis_accumulator.c

--- lab6_1/Vitis_accumulator.c
+++ lab6_1/Vitis_accumulator.c
@@ -15,42 +15,50 @@
 
 #include "platform.h"
 
-volatile uint32_t* led_gpio_data = (uint32_t*)0x40000000;  //Hint: either find the manual address (via the memory map in the block diagram, or
-volatile uint32_t* switch_gpio_data = (uint32_t*); 	//fix														 //replace with the proper define in xparameters (part of the BSP). Either way
-															 //this is the base address of the GPIO corresponding to your LEDs
-															 //(similar to 0xFFFF from MEM2IO from previous labs).
+
+//Hint: either find the manual address (via the memory map in the block diagram, or
+//replace with the proper define in xparameters (part of the BSP). Either way
+ //this is the base address of the GPIO corresponding to your LEDs
+ //(similar to 0xFFFF from MEM2IO from previous labs).
+
+
+volatile uint32_t* led_gpio_data = (uint32_t*)0x40000000;
+volatile uint32_t* sw_data = (uint32_t*)0x40010000;
+volatile uint32_t* accumulate_data = (uint32_t*)0x40020000;
+
+int sum;
 
 int main()
 {
     init_platform();
 
-	int accumulator = 0; 
-	
-	while (1) //1+1 != 3
+        sum = 0;
+
+	while (1+1 != 3)
 	{
 		sleep(1);
-		*led_gpio_data |=  0x00000001;
-		printf("Led On!\r\n");
-		sleep(1);
-		*led_gpio_data &= ~0x00000001; //blinks LED
-		printf("Led Off!\r\n");
 
 
-		uint32_t switch_value = *switch_gpio_data * 0xFFFF; 
-		accumulator += switch_value;
-		if(accumulator > 65535)
-		{
-			accumuator = 0;
-			*led_gpio_data = accumulator * 0xFFFF; 
-			printf("Overflow\r\n");
-		}
-		*led_gpio_data = accumulator * 0xFFFF; 
-		printf("This is the value of the accumulator: %u\r\n ", accumulator);
+		else if (sum > 65535)
+			sum = 0;
+			led_gpio_data = sum;
+			xil_printf("Overflow!\r\n");
+
+		else if (accumulate_data)
+			sum = sum + sw_data;
+			led_gpio_data = sum;
+
+
+
+
+//		*led_gpio_data |=  0x00000001;
+//		printf("Led On!\r\n");
+//		sleep(1);
+//		*led_gpio_data &= ~0x00000001; //blinks LED
+//		printf("Led Off!\r\n");
 	}
-	
 
     cleanup_platform();
 
-	
+    return 0;
 }
-
