813f0bc95eef21d6d0fcf5559ef7c966206593aa
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-10-31 10:22:20 -0500

    Update Color_Mapper.sv

--- Lab7.1/Color_Mapper.sv
+++ Lab7.1/Color_Mapper.sv
@@ -33,32 +33,32 @@ module  color_mapper ( input  logic [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
      of the 120 available multipliers on the chip!  Since the multiplicants are required to be signed,
 	  we have to first cast them from logic to int (signed by default) before they are multiplied). */
 	  
-    int DistX, DistY, Size;
-    assign DistX = DrawX - BallX;
-    assign DistY = DrawY - BallY;
-    assign Size = Ball_size;
+    // int DistX, DistY, Size;
+    // assign DistX = DrawX - BallX;
+    // assign DistY = DrawY - BallY;
+    // assign Size = Ball_size;
   
-    always_comb
-    begin:Ball_on_proc
-        if ( (DistX*DistX + DistY*DistY) <= (Size * Size) )
-            ball_on = 1'b1;
-        else 
-            ball_on = 1'b0;
-     end 
+    // always_comb
+    // begin:Ball_on_proc
+    //     if ( (DistX*DistX + DistY*DistY) <= (Size * Size) )
+    //         ball_on = 1'b1;
+    //     else 
+    //         ball_on = 1'b0;
+    //  end 
        
-    always_comb
-    begin:RGB_Display
-        if ((ball_on == 1'b1)) begin 
-            Red = 4'hf;
-            Green = 4'h7;
-            Blue = 4'h0;
-        end       
-        else begin 
-            Red = 4'hf - DrawX[9:6]; 
-            Green = 4'hf - DrawX[9:6];
-            Blue = 4'hf - DrawX[9:6];
-        end      
-    end 
+    // always_comb
+    // begin:RGB_Display
+    //     if ((ball_on == 1'b1)) begin 
+    //         Red = 4'hf;
+    //         Green = 4'h7;
+    //         Blue = 4'h0;
+    //     end       
+    //     else begin 
+    //         Red = 4'hf - DrawX[9:6]; 
+    //         Green = 4'hf - DrawX[9:6];
+    //         Blue = 4'hf - DrawX[9:6];
+    //     end      
+    // end 
     
 endmodule
 
