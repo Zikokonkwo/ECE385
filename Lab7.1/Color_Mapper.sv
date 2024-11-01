3f213f87468f9d882ea7d1b10ca33145f53d46d2
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-11-01 10:36:37 -0500

    Update Color_Mapper.sv

--- Lab7.1/Color_Mapper.sv
+++ Lab7.1/Color_Mapper.sv
@@ -20,47 +20,11 @@ module  color_mapper ( input  logic [9:0] DrawX, DrawY,
 		     );
     
     logic ball_on;
-	 
- /* Old Ball: Generated square box by checking if the current pixel is within a square of length
-    2*BallS, centered at (BallX, BallY).  Note that this requires unsigned comparisons.
-	 
-    if ((DrawX >= BallX - Ball_size) &&
-       (DrawX <= BallX + Ball_size) &&
-       (DrawY >= BallY - Ball_size) &&
-       (DrawY <= BallY + Ball_size))
-       )
+    logic [10:0] sprite_addr;
+    logic [7:0] font_data; 
 
-     New Ball: Generates (pixelated) circle by using the standard circle formula.  Note that while 
-     this single line is quite powerful descriptively, it causes the synthesis tool to use up three
-     of the 120 available multipliers on the chip!  Since the multiplicants are required to be signed,
-	  we have to first cast them from logic to int (signed by default) before they are multiplied). */
-	  
-    // int DistX, DistY, Size;
-    // assign DistX = DrawX - BallX;
-    // assign DistY = DrawY - BallY;
-    // assign Size = Ball_size;
-  
-    // always_comb
-    // begin:Ball_on_proc
-    //     if ( (DistX*DistX + DistY*DistY) <= (Size * Size) )
-    //         ball_on = 1'b1;
-    //     else 
-    //         ball_on = 1'b0;
-    //  end 
-       
-    // always_comb
-    // begin:RGB_Display
-    //     if ((ball_on == 1'b1)) begin 
-    //         Red = 4'hf;
-    //         Green = 4'h7;
-    //         Blue = 4'h0;
-    //     end       
-    //     else begin 
-    //         Red = 4'hf - DrawX[9:6]; 
-    //         Green = 4'hf - DrawX[9:6];
-    //         Blue = 4'hf - DrawX[9:6];
-    //     end      
-    // end 
+    logic invert;
+    logic [2:0] FGD_R, FGD_G, FGD_B, BKG_R, BKG_G, BKG_B;
 
    //psuedocode:
 	//Calculate glyph coordinates from pixel coordinates
@@ -87,8 +51,6 @@ module  color_mapper ( input  logic [9:0] DrawX, DrawY,
 	assign byte_row = byte_num / 80;
 	assign byte_col = byte_num % 80;
 	
-	logic invert;
-	logic [2:0] FGD_R, FGD_G, FGD_B, BKG_R, BKG_G, BKG_B;
 	
 	assign FGD_R = slv_regs[600][24:21];
 	assign FGD_G = slv_regs[600][20:17];
