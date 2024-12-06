136882b53eba0e504e515d28254e5073d84e1088
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-12-06 14:20:59 -0600

    Update CollisionDetection.sv

--- Project/CollisionDetection.sv
+++ Project/CollisionDetection.sv
@@ -2,9 +2,9 @@ mdoule collision_detection(  input wire [9:0] player_x,    // Player's x positio
     input [9:0] player_y,    // Player's y position
     input [9:0] obstacle_x,  // Obstacle's x position
     input [9:0] obstacle_y,  // Obstacle's y position
-    input [9:0] player_width,  // Player's width (e.g., 16)
-    input [9:0] player_height, // Player's height (e.g., 16)
-    input [9:0] obstacle_width, // Obstacle's width (e.g., 32)
-    input [9:0] obstacle_height, // Obstacle's height (e.g., 32)
+    input [9:0] player_width,  // Player's width 
+    input [9:0] player_height, // Player's height 
+    input [9:0] obstacle_width, // Obstacle's width 
+    input [9:0] obstacle_height, // Obstacle's height 
     output reg collision_detected);
 endmodule
