b6f481ded8894dda68e0c086650b8d0bb9fb4777
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-12-09 17:46:50 -0600

    Update ball.sv

--- Project/ball.sv
+++ Project/ball.sv
@@ -15,131 +15,114 @@
 //-------------------------------------------------------------------------
 
 
-module  ball 
+module  obstacle 
  ( 
     input  logic        Reset, 
     input  logic        frame_clk,
-    input  logic [7:0]  keycode,
+ 
 
-    output logic [9:0]  BallX, 
-    output logic [9:0]  BallY, 
-    output logic [9:0]  BallS 
+   output logic [9:0]  OBSX, 
+   output logic [9:0]  OBSY, 
+   output logic [9:0]  OBS_size 
 );
     
 
 	 
-    parameter [9:0] Ball_X_Center=320;  // Center position on the X axis
-    parameter [9:0] Ball_Y_Center=240;  // Center position on the Y axis
-    parameter [9:0] Ball_X_Min=0;       // Leftmost point on the X axis
-    parameter [9:0] Ball_X_Max=639;     // Rightmost point on the X axis
-    parameter [9:0] Ball_Y_Min=0;       // Topmost point on the Y axis
-    parameter [9:0] Ball_Y_Max=479;     // Bottommost point on the Y axis
-    parameter [9:0] Ball_X_Step=1;      // Step size on the X axis
-    parameter [9:0] Ball_Y_Step=1;      // Step size on the Y axis
+    parameter [9:0] OBS_X_Center=320;  // Center position on the X axis
+    parameter [9:0] OBS_Y_Center=240;  // Center position on the Y axis
+    parameter [9:0] OBS_X_Min=0;       // Leftmost point on the X axis
+    parameter [9:0] OBS_X_Max=639;     // Rightmost point on the X axis
+    parameter [9:0] OBS_Y_Min=0;       // Topmost point on the Y axis
+    parameter [9:0] OBS_Y_Max=479;     // Bottommost point on the Y axis
+    parameter [9:0] OBS_X_Step=1;      // Step size on the X axis
+    parameter [9:0] OBS_Y_Step=1;      // Step size on the Y axis
 
-    logic [9:0] Ball_X_Motion;
-    logic [9:0] Ball_X_Motion_next;
-    logic [9:0] Ball_Y_Motion;
-    logic [9:0] Ball_Y_Motion_next;
+    logic [9:0] OBS_X_Motion;
+    logic [9:0] OBS_X_Motion_next;
+    logic [9:0] OBS_Y_Motion;
+    logic [9:0] OBS_Y_Motion_next;
 
-    logic [9:0] Ball_X_next;
-    logic [9:0] Ball_Y_next;
+    logic [9:0] OBS_X_next;
+    logic [9:0] OBS_Y_next;
 
     // typedef enum logic [1:0] {IDLE, UP, DOWN, LEFT, RIGHT} motion_t;
     // motion_t current_direction, next_direction;
 
     // Internal signals for motion and position updates
-//    logic [9:0] Ball_X_Motion, Ball_X_Motion_next;
-//    logic [9:0] Ball_Y_Motion, Ball_Y_Motion_next;
-//    logic [9:0] Ball_X_next, Ball_Y_next;
+//    logic [9:0] OBS_X_Motion, OBS_X_Motion_next;
+//    logic [9:0] OBS_Y_Motion, OBS_Y_Motion_next;
+//    logic [9:0] OBS_X_next, OBS_Y_next;
 
     always_comb begin
-        Ball_Y_Motion_next = Ball_Y_Motion; // set default motion to be same as prev clock cycle 
-        Ball_X_Motion_next = Ball_X_Motion;
-       // next_direction = current_direction;  // Default to current direction
+        OBS_Y_Motion_next = OBS_Y_Motion; // set default motion to be same as prev clock cycle 
+        OBS_X_Motion_next = OBS_X_Motion;
 
-        // Direction control based on key press (no diagonal allowed)
-  //       case (current_direction)
-  //           IDLE: begin
-		// if (keycode == 8'h26) next_direction = UP;    // Up arrow key
-		// else if (keycode == 8'h22) next_direction = DOWN;  // Down arrow key
-		// else if (keycode == 8'h04) next_direction = LEFT;  // Left arrow key
-		// else if (keycode == 8'h07) next_direction = RIGHT; // Right arrow key
-  //           end
-
-  //           UP: Ball_Y_Motion_next = -Ball_Y_Step;   // Move up
-  //           DOWN: Ball_Y_Motion_next = Ball_Y_Step;  // Move down
-  //           LEFT: Ball_X_Motion_next = -Ball_X_Step; // Move left
-  //           RIGHT: Ball_X_Motion_next = Ball_X_Step; // Move right
-  //       endcase
-	
-
-        //modify to control ball motion with the keycode
-	if (keycode == 8'h1A) //go up
-	begin
-            	Ball_Y_Motion_next = -10'd1;
-	    	Ball_X_Motion_next = 0;
-	end
+        //modify to control OBS motion with the keycode
+	// if (keycode == 8'h1A) //go up
+	// begin
+ //            	OBS_Y_Motion_next = -10'd1;
+	//     	OBS_X_Motion_next = 0;
+	// end
 	    
-	else if(keycode == 8'h16) //go down
-	begin
-	    	Ball_Y_Motion_next = 10'd1;
-	    	Ball_X_Motion_next = 0;
-	end 
-	    else if(keycode == 8'h04) //move left
-	begin
-		Ball_Y_Motion_next = 0;
-		Ball_X_Motion_next = -10'd1;
-	end 
-	    else if(keycode == 8'h07) //move right
-	begin
-		Ball_Y_Motion_next = 0;
-	    	Ball_X_Motion_next = 10'd1;
-	end 
+	// else if(keycode == 8'h16) //go down
+	// begin
+	//     	OBS_Y_Motion_next = 10'd1;
+	//     	OBS_X_Motion_next = 0;
+	// end 
+	//     else if(keycode == 8'h04) //move left
+	// begin
+	// 	OBS_Y_Motion_next = 0;
+	// 	OBS_X_Motion_next = -10'd1;
+	// end 
+	//     else if(keycode == 8'h07) //move right
+	// begin
+	// 	OBS_Y_Motion_next = 0;
+	//     	OBS_X_Motion_next = 10'd1;
+	// end 
 
-        if ( (BallY + BallS) >= Ball_Y_Max )  // Ball is at the bottom edge, BOUNCE!
+	    if ( (OBSY + OBS_size) >= OBS_Y_Max )  // OBS is at the bottom edge, BOUNCE!
         begin
-            Ball_Y_Motion_next = (~ (Ball_Y_Step) + 1'b1);  // set to -1 via 2's complement.
+            OBS_Y_Motion_next = (~ (OBS_Y_Step) + 1'b1);  // set to -1 via 2's complement.
         end
-        else if ( (BallY - BallS) <= Ball_Y_Min )  // Ball is at the top edge, BOUNCE!
+	    else if ( (OBSY - OBS_size) <= OBS_Y_Min )  // OBS is at the top edge, BOUNCE!
         begin
-            Ball_Y_Motion_next = Ball_Y_Step;
+            OBS_Y_Motion_next = OBS_Y_Step;
         end  
        //fill in the rest of the motion equations here to bounce left and right
-	if ((BallX + BallS ) >= Ball_X_Max) //right edge --> bounce
+	    if ((OBSX + OBS_size ) >= OBS_X_Max) //right edge --> bounce
 	begin
-		Ball_X_Motion_next = (~ (Ball_X_Step) + 1'b1);  // set to -1 via 2's complement.
+		OBS_X_Motion_next = (~ (OBS_X_Step) + 1'b1);  // set to -1 via 2's complement.
 	end
-	    else if ((BallX - BallS ) <= Ball_X_Min) //left edge --> bounce
+	    else if ((OBSX - OBS_size ) <= OBS_X_Min) //left edge --> bounce
 	begin
-		Ball_X_Motion_next = Ball_X_Step;
+		OBS_X_Motion_next = OBS_X_Step;
 	end
 	    
 
     end
 
-    assign BallS = 16;  // default ball size
-    assign Ball_X_next = (BallX + Ball_X_Motion_next);
-    assign Ball_Y_next = (BallY + Ball_Y_Motion_next);
+    assign OBS_size = 16;  // default OBS size
+    assign OBS_X_next = (OBSX + OBS_X_Motion_next);
+    assign OBS_Y_next = (OBSY + OBS_Y_Motion_next);
    
     always_ff @(posedge frame_clk) //make sure the frame clock is instantiated correctly
     begin: Move_Ball
         if (Reset)
         begin 
-            Ball_Y_Motion <= 10'd0; //Ball_Y_Step;
-			Ball_X_Motion <= 10'd1; //Ball_X_Step;
+            OBS_Y_Motion <= 10'd0; //OBS_Y_Step;
+			OBS_X_Motion <= 10'd1; //OBS_X_Step;
             
-			BallY <= Ball_Y_Center;
-			BallX <= Ball_X_Center;
+			OBSY <= OBS_Y_Center;
+			OBSX <= OBS_X_Center;
         end
         else 
         begin 
 
-			Ball_Y_Motion <= Ball_Y_Motion_next; 
-			Ball_X_Motion <= Ball_X_Motion_next; 
+			OBS_Y_Motion <= OBS_Y_Motion_next; 
+			OBS_X_Motion <= OBS_X_Motion_next; 
 
-            BallY <= Ball_Y_next;  // Update ball position
-            BallX <= Ball_X_next;
+            OBSY <= OBS_Y_next;  // Update OBS position
+            OBSX <= OBS_X_next;
 			
 		end  
     end
