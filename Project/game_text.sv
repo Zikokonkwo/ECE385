656124cbc54fc4beba22141c6542d7c5d0fd27af
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-12-09 14:43:38 -0600

    Update game_text.sv

--- Project/game_text.sv
+++ Project/game_text.sv
@@ -1,1 +1,15 @@
+module game_text(
+    input clk,
+    input [3:0] dig0, dig1,
+    input [9:0] drawX, drawY,
+    output text_on,
+  output reg [11:0] text_rgb
+);
 
+//Display Fail Region
+
+//Display Level Region
+
+
+  
+endmodule
