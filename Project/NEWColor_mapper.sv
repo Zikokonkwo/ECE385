3649fbfa9179f4319f341a876947b5b70e2ab2e6
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-12-11 19:04:18 -0600

    Update NEWColor_mapper.sv

--- Project/NEWColor_mapper.sv
+++ Project/NEWColor_mapper.sv
@@ -25,7 +25,79 @@ module  color_mapper ( input  logic [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
     logic obs_on;
      logic obs_on2;
     ////
-	 
+
+////////////////////////////////////////////SQUARE
+parameter X_MIN = 39;                  // left border of display area
+parameter X_MAX = 599;                  // right border of display area
+parameter Y_MAX = 479;                  // bottom border of display area
+parameter Y_MIN = 0;                  // top border of display area
+ parameter SQUARE_SIZE = 16;             // width of square sides in pixels
+parameter SQUARE_VELOCITY_POS = 2;      // set position change value for positive direction
+parameter SQUARE_VELOCITY_NEG = -2;     // set position change value for negative direction  
+
+wire refresh_tick;
+assign refresh_tick = ((DrawY == 481) && (DrawX == 0)) ? 1 : 0;
+
+ // square boundaries and position
+    wire [9:0] sq_x_l, sq_x_r;              // square left and right boundary
+    wire [9:0] sq_y_t, sq_y_b;              // square top and bottom boundary
+    
+    reg [9:0] sq_x_reg, sq_y_reg;           // regs to track left, top position
+    wire [9:0] sq_x_next, sq_y_next;        // buffer wires
+    
+    reg [9:0] x_delta_reg, y_delta_reg;     // track square speed
+    reg [9:0] x_delta_next, y_delta_next;   // buffer regs    
+    
+    // register control
+    always @(posedge clk or posedge reset)
+        if(reset) begin
+            sq_x_reg <= 0;
+            sq_y_reg <= 0;
+            x_delta_reg <= 10'h002;
+            y_delta_reg <= 10'h002;
+        end
+        else begin
+            sq_x_reg <= sq_x_next;
+            sq_y_reg <= sq_y_next;
+            x_delta_reg <= x_delta_next;
+            y_delta_reg <= y_delta_next;
+        end
+    
+    // square boundaries
+    assign sq_x_l = sq_x_reg;                   // left boundary
+    assign sq_y_t = sq_y_reg;                   // top boundary
+    assign sq_x_r = sq_x_l + SQUARE_SIZE - 1;   // right boundary
+    assign sq_y_b = sq_y_t + SQUARE_SIZE - 1;   // bottom boundary
+    
+    // square status signal
+    wire sq_on;
+    assign sq_on = (sq_x_l <= x) && (x <= sq_x_r) &&
+                   (sq_y_t <= y) && (y <= sq_y_b);
+                   
+    // new square position
+    assign sq_x_next = (refresh_tick) ? sq_x_reg + x_delta_reg : sq_x_reg;
+    assign sq_y_next = (refresh_tick) ? sq_y_reg + y_delta_reg : sq_y_reg;
+    
+    // new square velocity 
+    always @* begin
+        x_delta_next = x_delta_reg;
+        y_delta_next = y_delta_reg;
+        if(sq_y_t < 1)                              // collide with top display edge
+            y_delta_next = SQUARE_VELOCITY_POS;     // change y direction(move down)
+        else if(sq_y_b > Y_MAX)                     // collide with bottom display edge
+            y_delta_next = SQUARE_VELOCITY_NEG;     // change y direction(move up)
+        else if(sq_x_l < 1)                         // collide with left display edge
+            x_delta_next = SQUARE_VELOCITY_POS;     // change x direction(move right)
+        else if(sq_x_r > X_MAX)                     // collide with right display edge
+            x_delta_next = SQUARE_VELOCITY_NEG;     // change x direction(move left)
+    end
+    
+
+    
+
+
+
+	
  /* Old Ball: Generated square box by checking if the current pixel is within a square of length
     2*BallS, centered at (BallX, BallY).  Note that this requires unsigned comparisons.
 	 
@@ -139,7 +211,13 @@ always_comb begin
             Green = 4'hf;
             Blue = 4'h0;
        end
-            else if ((obs_on || obs_on2) == 1'b1) begin
+	if(sq_on) 
+		begin
+               	  Red = 4'h0;
+        	  Green = 4'hf;
+                  Blue = 4'h0;
+	 end
+	 if (obs_on == 1'b1) begin //((obs_on || obs_on2) == 1'b1)
              Red = foreground-(4'hf);
             Green = foreground-(4'h1);
             Blue = foreground-(4'h1);
