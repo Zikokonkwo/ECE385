29880f2b8e707cdafb395555bfc2084ce7225a53
JasonHart922 <113485657+JasonHart922@users.noreply.github.com>
2024-12-12 20:57:12 -0600

    Update NEWGame_controller.sv

--- Project/NEWGame_controller.sv
+++ Project/NEWGame_controller.sv
@@ -4,8 +4,8 @@ module game_state_machine (
     input logic clk, reset, ObsX, ObsY, Ball_size, finish_line_reached, collision,
     output logic [3:0] foreground, background,
     output logic [1:0] current_level, speed, obstacle_count,
-    output logic reset_level, reset_player,
-    //output logic [9:0] BallX, BallY
+    output logic reset_level, reset_player, speedX, speedY
+//    output logic [9:0] BallX, BallY
 );
     game_state_t state, next_state;
 
@@ -14,66 +14,70 @@ logic sprite_collision;
 
 
 
-//     // State Transition Logic
-//     always_ff @(posedge clk or posedge reset) begin
-// 	    if (reset || reset_level) begin
-//             state <= LEVEL1;
+    // State Transition Logic
+    always_ff @(posedge clk or posedge reset) begin
+	    if (reset || reset_level) begin
+            state <= LEVEL1;
             
-// //            BallX <= 20;//ADDED
-// //            BallY <= 240;//ADDED
+//            BallX <= 20;//ADDED
+//            BallY <= 240;//ADDED
  
-// //	    else if ((BallX + Ball_size > ObsX - Ball_size) && (BallX - Ball_size < ObsX + Ball_size) && 
-// //            (BallY + Ball_size > ObsY - Ball_size) && (BallY - Ball_size < ObsY + Ball_size)) 
-// //	    	sprite_collision <= 1'b1; // sprite collided with obstacle and must reset
+//	    else if ((BallX + Ball_size > ObsX - Ball_size) && (BallX - Ball_size < ObsX + Ball_size) && 
+//            (BallY + Ball_size > ObsY - Ball_size) && (BallY - Ball_size < ObsY + Ball_size)) 
+//	    	sprite_collision <= 1'b1; // sprite collided with obstacle and must reset
 	    	
 	   
-//          end else if (reset_player) begin
-//                      BallX <= 20;//ADDED
-//                      BallY <= 240;//ADDED
+//         end else if (reset_player) begin
+//                     BallX <= 20;//ADDED
+//                     BallY <= 240;//ADDED
          
-//          end
-//          else
-// 	  			 state <= next_state;
-//     end
-	
+//         end
+         end else
+	  			 state <= next_state;
+    end
 //State control signals/////////////////////////
 
     always_comb
 	begin 
 		// Default controls signals
-		speed = 2'b00; //clears obstacle speed until defined in each level
-        	background = 4'b0001; // defaults the background color to use 0 as its color data reference
+		speedX = 2'b01; //clears obstacle speed until defined in each level
+		speedY = 2'b01; //obstacle speed is twice the initial speed
+        	background = 4'b0101; // defaults the background color to use 0 as its color data reference
         	foreground= 4'b0001; // defaults the foreground color to use 0 as its color data reference
        	 	obstacle_count = 2'b00; // initializes the projectile count multiplier to 0
 
 		case (state)
 			LEVEL1 : 
 				begin 
-		                speed = 2'b01; //obstacle speed is initially slow
+		                speedX = 2'b10; //obstacle speed is initially slow
+		                speedY = 2'b01; //obstacle speed is twice the initial speed
                         	background = 4'b0011; // sets the background color to use 1 as its color data reference
-                        	foreground= 4'b0011; // sets the foreground color to use 1 as its color data reference
+                        	foreground= 4'b0010; // sets the foreground color to use 1 as its color data reference
                         	obstacle_count = 2'b01; // obstacle count multiplier is initially 1
             
 				end
 			LEVEL2 : 
-				begin 
-		                speed = 2'b10; //obstacle speed is twice the initial speed
-                        	background = 4'b0110; // sets the background color to use 2 as its color data reference
-                        	foreground= 4'b0110; // sets the foreground color to use 2 as its color data reference
+				begin //000000000000000000  MADE COLOR SAME AS LEVEL BEOFER TO CEHCK 00000000000000000000000000000000000000000000000000
+		                speedX = 2'b01; //obstacle speed is twice the initial speed
+		                 speedY = 2'b11; //obstacle speed is twice the initial speed
+                        	background = 4'b0101; // sets the background color to use 2 as its color data reference
+                        	foreground= 4'b0100; // sets the foreground color to use 2 as its color data reference
                         	obstacle_count = 2'b10; // obstacle count multiplier is 2
                         
 				end
 			LEVEL3 : 
 				begin 
-		                speed = 2'b11; //obstacle speed is three times the initial speed 
-                        	background = 4'b1010; // sets the background color to use 4 as its color data reference
-                        	foreground= 4'b1010; // sets the foreground color to use 4 as its color data reference
+		                speedX = 2'b11; //obstacle speed is three times the initial speed 
+		                speedY = 2'b11; //obstacle speed is twice the initial speed
+                        	background = 4'b1110; // sets the background color to use 4 as its color data reference
+                        	foreground= 4'b1000; // sets the foreground color to use 4 as its color data reference
                         	obstacle_count = 2'b11; // obstacle count multiplier is 3
                         
 				end
 			GAME_OVER : 
 				begin 
-                        	speed = 2'b00; // halts obstacles when game ends
+                        	speedX = 2'b00; // halts obstacles when game ends
+                        	speedY = 2'b00; //obstacle speed is twice the initial speed
                         	background = 4'b0000; // sets the background color to use 8 as its color data reference
                         	foreground= 4'b0000; // sets the foreground color to use 8 as its color data reference
                     		obstacle_count = 2'b00; // clear obstacle count multiplier when game ends
@@ -88,6 +92,9 @@ logic sprite_collision;
     always_comb begin
        
         next_state = state; // Default to current state 
+
+
+
 //     reset_level = 0;    // Default no reset  //move this into each else statement fo the levels????????????????????????????
 
 
@@ -100,8 +107,9 @@ logic sprite_collision;
                     next_state = LEVEL1;
                      // Reset the level
                 end else if (finish_line_reached)begin
-                    next_state = LEVEL2; // Progress to Level 2
                     reset_player = 1;
+                    next_state = LEVEL2; // Progress to Level 2
+                    
                     end
 		    else
 			    next_state = LEVEL1;
@@ -112,8 +120,9 @@ logic sprite_collision;
                     reset_level = 1;
                     next_state = LEVEL1;
                 end else if (finish_line_reached)begin
+                  reset_player = 1;
                     next_state = LEVEL3;
-                    reset_player = 1;
+                  
 		   	end else
 			     next_state = LEVEL2;
 			     reset_player = 0;
