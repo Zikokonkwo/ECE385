5a3462238510c80033ff4182949e45e638f4e3cc
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-12-14 10:13:55 -0600

    Update Current_Color_Mapper.sv

--- Project/Current_Color_Mapper.sv
+++ Project/Current_Color_Mapper.sv
@@ -17,7 +17,7 @@ module color_mapper (
     input logic [9:0] BallX, BallY, DrawX, DrawY, Ball_size, ObsX, ObsY, ObsX2, ObsY2,
     input logic [3:0] foreground, background,
     input logic [1:0] current_level,
-    input logic clk, reset,
+    input logic frame_clk, Reset,
     output logic [3:0] Red, Green, Blue,
     output logic finish_line_reached, reset_player, collision, collision2
 );
@@ -28,8 +28,8 @@ module color_mapper (
     logic [9:0] SquareSize = 10'd20; // Size of the square
 
     // Initialize square direction and position
-    always_ff @(posedge clk or posedge reset) begin
-        if (reset) begin
+    always_ff @(posedge frame_clk ) begin
+        if (Reset) begin
             SquareX <= 10'd300;
             SquareY <= 10'd200;
             Square_X_Motion <= 10'd1; // Initial motion in X
